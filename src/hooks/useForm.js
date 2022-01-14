052c684804b9f6c4fdd1d9b62188d690f9c8a216
brandib222 <brandi.danielle.ball@gmail.com>
2022-01-14 12:35:01 -0500

    created the useForm hook

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,1 +1,9 @@
 // write your custom hook here to control your checkout form
+import { useState } from 'react';
+
+const useForm = (initialValue) => {
+    const [state, setState] = useState(initialValue);
+    return [state, setState];
+}
+
+export default useForm;
